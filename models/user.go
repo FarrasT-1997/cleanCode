075b9a6cd347f27af27619589df1c25b0c3db9e7
Farras Timorremboko <Farras>
2021-07-22 15:59:30 +0700

    Merubah kolom author menjadi penulis

--- models/user.go
+++ models/user.go
@@ -14,5 +14,5 @@ type Book struct {
 	gorm.Model
 	Book    string `json:"book" form:"book"`
 	Country string `json:"country" form:"country"`
-	Author  string `json:"author" form:"author"`
+	Penulis string `json:"author" form:"author"`
 }
